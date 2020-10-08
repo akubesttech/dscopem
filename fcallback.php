ebaa9145e54e29bed8155ab6925448234ce96ca7
akubesttech <37868807+akubesttech@users.noreply.github.com>
2020-10-07 17:45:45 -1100

    update dscopem

--- fcallback.php
+++ fcallback.php
@@ -37,16 +37,42 @@ if(!$tranx->status){
 $pstart = "1";
 $payamount = $tranx->data->amount / 100;
 $refme = $tranx->data->reference ;
+$queryshoolp= mysqli_query($condb,"select * from schoolsetuptd ")or die(mysqli_error($condb));
+							  $rowp = mysqli_fetch_array($queryshoolp);
+							  $ev_actives = $rowp['emailver']; $schoolNe = $rowp['SchoolName'];
   $qpayn = mysqli_query($condb,"SELECT * FROM payment_tb WHERE  trans_id ='".safee($condb,$refme)."'");
-	$hpeno = mysqli_fetch_array($qpayn); $feetp1 = $hpeno['fee_type']; $appnon = $hpeno['app_no'];
+	$hpeno = mysqli_fetch_array($qpayn); $feetp1 = $hpeno['fee_type']; $appnon = $hpeno['app_no']; $smatno  = $hpeno['RegNo'];
+    $status = "TRUE"; $sessionad  = $hpeno['session']; $department  = $hpeno['department']; $pro = $hpeno['prog'];
+    $studentRegno = getmatno($sessionad,$department,$pro); $regcount = "1".getlstr($studentRegno,3);  $p_email = $hpeno['email'];
+    $paid = $feeinfo['dueamount'];
+    $pass_word = substr(md5($studentRegno.SUDO_M),14);
 if('success' == $tranx->data->status){
   // transaction was successful...
   // please check other things like whether you already gave value for this ref
   // if the email matches the customer who owns the product etc
   // Give value
-$sql2_up = mysqli_query($condb,"UPDATE payment_tb SET pay_status='1',paid_amount ='".safee($condb,$payamount)."' WHERE trans_id ='".safee($condb,$refme)."' ")or die(mysqli_error($condb));
-  $sql2_up1 = mysqli_query($condb,"UPDATE feecomp_tb SET pstatus = '1' WHERE Batchno ='".safee($condb,$feetp1)."' ")or die(mysqli_error($condb));
+ $sql2_up = mysqli_query($condb,"UPDATE payment_tb SET pay_status='1',paid_amount ='".safee($condb,$paid)."',stud_reg = '".safee($condb,$studentRegno)."' WHERE trans_id ='".safee($condb,$refme)."' ")or die(mysqli_error($condb));
+  $sql2_up1 = mysqli_query($condb,"UPDATE feecomp_tb SET pstatus = '1',regno = '".safee($condb,$studentRegno)."' WHERE Batchno ='".safee($condb,$feetp1)."'")or die(mysqli_error($condb));
+ if($smatno < 1){
+  $sql2_up3 = mysqli_query($condb,"UPDATE student_tb SET RegNo='".safee($condb,$studentRegno)."',reg_count='".safee($condb,$regcount)."',verify_Data = '".safee($condb,$status)."',password = '".safee($condb,$pass_word)."' WHERE appNo = '".safee($condb,$appnon)."'")or die(mysqli_error($condb));
+
   //echo "<h2>Thank you for making a purchase. Your file has bee sent your email.</h2>"
+  $msg = nl2br("Congratulations! ".getname($appnon).",.\n
+	
+	Following your Successful School fees payment of ".number_format($paid,2)." your Matric Number was Generated .\n
+	Find below your Matric Number and your CMS Password \n
+	Matric Number :".$studentRegno.   "\n
+	Password :".$studentRegno.   "\n
+	..................................................................\n
+    Please Login and reset your Password!\n
+    
+    This Message was Sent From " .$schoolNe ." @ ".$_SERVER['HTTP_HOST']." dated ".date('d-m-Y').".\n
+    For inquiry and complaint please email info@smartdelta.com.ng \n
+	
+	Thank You Admin!\n\n");
+  $subject="Matric Number Notification";
+  $mail_data = array('to' => $p_email, 'sub' => $subject, 'msg' => 'Notify','body' => $msg, 'srname' => $comn);
+	send_email2($mail_data); }
   	message("Your Payment was Successful!", "success");
 		        redirect('apply_b.php?view=e_view&id='.md5($refme));
    
