580bc603df113b973a8e44aaf1e30a00b2551a8d
akubesttech <37868807+akubesttech@users.noreply.github.com>
2020-11-17 16:21:21 -1100

    ds_217

--- e_payview.php
+++ e_payview.php
@@ -7,11 +7,12 @@ $paystatus12=mysqli_num_rows($paystatus1);
 if($paystatus12 < 1){ message("The page you are trying to access is not Available.", "error");
 redirect('apply_b.php?view=opay'); }
 $dform_get2 = mysqli_fetch_assoc($paystatus1); $ftranid = $dform_get2['trans_id']; $sname = $dform_get2['FirstName'];$fname = $dform_get2['SecondName']; $oname = $dform_get2['Othername']; $fullname = $sname." ".$fname." ".$oname; $appNo = $dform_get2['appNo']; $feename = $dform_get2['ft_cat'];
-$femail = $dform_get2['email']; $fphone = $dform_get2['phone']; $fprog = $dform_get2['prog'];$fsession = $dform_get2['session']; $pamount = $dform_get2['paid_amount'];
+$femail = $dform_get2['email']; $fphone = $dform_get2['phone']; $fprog = $dform_get2['prog'];$fsession = $dform_get2['session']; $pamount = $dform_get2['paid_amount']; $matno = $dform_get2['stud_reg'];
 $feetp = $dform_get2['fee_type']; $famount  = $dform_get2['dueamount']; $dategen  = $dform_get2['dategen']; $Pmode  = $dform_get2['pay_mode'];
 $date20 = str_replace('/', '-', $dform_get2['pay_date'] );  $newDate20 = date("Y-m-d", strtotime($date20));
    $timestamp = strtotime($newDate20); $datetime	= date('l, jS F Y', $timestamp);
-$paycomponent=mysqli_query($condb,"SELECT * FROM feecomp_tb  WHERE Batchno ='".safee($condb,$feetp)."' "); $serial=1;		
+$paycomponent=mysqli_query($condb,"SELECT * FROM feecomp_tb  WHERE Batchno ='".safee($condb,$feetp)."' "); $serial=1;
+	
 ?>
    <section id="content" role="document">
         <main style="min-height: 168px;">
@@ -39,12 +40,16 @@ $paycomponent=mysqli_query($condb,"SELECT * FROM feecomp_tb  WHERE Batchno ='".s
             <div class="row">
                 <div class="col-xs-12">
             <h3><font size="4"><?php if(empty($_GET['id'])){ ?> Payment Information Preview <?php }else{ ?>
-		<?php if(substr($dform_get2['fee_type'],0,1) == "B"){echo strtoupper($ptitle); }else{  echo strtoupper($ptitle2); } }?>
+		<?php if(substr($dform_get2['fee_type'],0,1) == "B"){echo strtoupper($ptitle); }else{  echo strtoupper($ptitle2); } }
+		if(strlen($matno) > 0){ //$smartcharge = 4000 ;
+		$note = " Your Matric No : <strong>$matno</strong>  <br>also Note that your Matric No Has been Mailed to you. "; }else{ $note = ""; //$smartcharge = 500 ;
+        }
+		?>
 			</font> </h3>
         </div>
         <div class="col-xs-12 primary-content link-icons">
 <p class="first-paragraph"><?php if(empty($_GET['id'])){ ?><font color="red" size="4">Please Note That a debit card will be required at this stage to complete your payment.<br></font> <?php }else{ ?>
-<font color="green" size="4">Your <?php echo getfeecat($feename); ?> Payment was Successful,<br></font> <?php } ?>
+<font color="green" size="4">Your <?php echo getfeecat($feename); ?> Payment was Successful,<br><?php echo $note; ?></font> <?php } ?>
 </p>
                 </div>
                 
@@ -76,13 +81,14 @@ $paycomponent=mysqli_query($condb,"SELECT * FROM feecomp_tb  WHERE Batchno ='".s
              <input type='hidden' name='fail_url' value='http://www.ucnettechnologies.net/failed.php' /> --!>
             <input type='hidden' name='emailx' value='<?php echo $femail;?>' /> 
 			<input type='hidden' name='total' value='<?php echo $famount ;?>' />
+            <input type='hidden' name='ft_cat' value='<?php echo $feename;?>' />
 			<div class="form_box">
 			 <div class="clear" style="overflow: auto;">
-        <table  border="1">
-       <tr class="row1">
-  <td width="20%" colspan="1" height="15"><strong> Application Number:</strong></td>
-    <td width="31%" colspan="4"><?php echo $appNo; ?></td>
-    </tr>
+        <table  border="1"><?php if(strlen($matno) > 0){ ?>
+       <tr class="row1"><td width="20%" colspan="1" height="15"><strong> Matric Number:</strong></td>
+    <td width="31%" colspan="4"><?php echo $matno; ?></td></tr><?php }else{ ?>
+    <tr class="row1"><td width="20%" colspan="1" height="15"><strong> Application Number:</strong></td>
+    <td width="31%" colspan="4"><?php echo $appNo; ?></td></tr><?php } ?>
   <tr class="row2">
   <td width="20%" colspan="1" height="15"><strong> Full Name:</strong></td>
     <td width="31%" colspan="4"><?php echo ucwords($fullname); ?></td>
